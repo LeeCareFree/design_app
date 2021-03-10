3942846e30c168748f0cdc32b7108cd666a03862
Ricardo <44264239+Ricardoltao@users.noreply.github.com>
2021-03-10 15:19:40 +0800

    更改全局状态栏，及加入首页轮播图

--- lib/config/service_api.dart
+++ lib/config/service_api.dart
@@ -1,5 +1,8 @@
-const serviceUrl = 'http://192.168.0.103:3000/api/';
+// const serviceUrl = 'http://192.168.0.103:3000/api/';
+const serviceUrl = 'http://192.168.0.104:3000/api/';
+
 const servicePath = {
   'login': serviceUrl + 'users/login',
   'register': serviceUrl + 'users/register',
+  'slideshow': serviceUrl + 'homes/slideshow'
 };
