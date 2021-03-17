a11aac5bcad9e3376c3cd1b5dc7a515095ce9cd6
Ricardo <44264239+Ricardoltao@users.noreply.github.com>
2021-03-17 13:08:06 +0800

    更改文章列表组件，联调后端接口

--- lib/config/service_api.dart
+++ lib/config/service_api.dart
@@ -1,4 +1,4 @@
-const serviceUrl = 'http://localhost:3000/api/';
+const serviceUrl = 'http://192.168.0.102:3000/api/';
 const servicePath = {
   'login': serviceUrl + 'users/login',
   'register': serviceUrl + 'users/register',
